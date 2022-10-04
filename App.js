53ec7ff1a0c9631ada6306721515bc2167cd0a7f
mercheroberto02@gmail.com <mercheroberto@gmail.com>
2022-10-03 19:54:44 -0600

    Details View

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import ListUser from "./views/ListUser";
 import CreateUser from "./views/CreateUser";
-//import DetailUser from "./views/DetailUser";
+import DetailUser from "./views/DetailUser";
 
 
 const Stack = createNativeStackNavigator()
@@ -16,7 +16,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name ="Lista de Usuarios" component={ListUser} options={{title: "Listado de Usuarios"}} />
         <Stack.Screen name ="CreateUser" component={CreateUser} options={{title: "Crear Usuario"}}/>
-        {/* <Stack.Screen name ="Detalle de Usuarios" component={DetailUser}/> */}
+        <Stack.Screen name ="DetailUser" component={DetailUser} options={{title:"Detalles del usuario"}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- views/DetailUser.jsx
+++ views/DetailUser.jsx
@@ -1,19 +1,50 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, {useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { collection, getDocs, doc, getDoc } from "firebase/firestore";
+import db from "../database/firebase9";
+import { useFocusEffect } from '@react-navigation/native';
+import { Center, Container, Heading, HStack, NativeBaseProvider, VStack } from "native-base";
 
-const DetailUser = () => {
-    return (
-        <View>
-            <Text style={styles.Text}>Listado de Usuarios</Text>
-        </View>
-    )
+export default function DetailUser({ navigation, route }) {
+  const [user, setUser] = useState([]);
+  const {userId}=route.params;
+  useFocusEffect(
+    React.useCallback(() => {
+      const obtenerDetallesUser = async () => {
+        const docRef = doc(db, "users", userId);
+        const docSnap = await getDoc(docRef);
+        if(docSnap.exists()){
+            console.log(docSnap.data());
+            setUser(docSnap.data())
+        }else{
+            console.log("No existe")
+        }
+      }
+
+      obtenerDetallesUser();
+    }, [])
+  );
+
+  return (
+    <NativeBaseProvider>
+        <Center>
+            <Container>
+                <Heading mt={5}>
+                    <Text>{user.name}</Text>
+                </Heading>
+                <VStack space={2}>
+                    <Text>{user.email}</Text>
+                    <Text>{user.phone}</Text>
+                </VStack>
+            </Container>
+        </Center>
+    </NativeBaseProvider>
+  );
 }
 
 const styles = StyleSheet.create({
-    Text: {
-        fontSize: 25,
-        color: 'cyan'
-    }
-})
-
-export default DetailUser
+  Text: {
+    fontSize: 25,
+    color: "cyan",
+  },
+});
